931fb5eba4837bbeaeb1960b2777bef8d39769ac
GudMeong <avada758@gmail.com>
2020-02-18 21:41:01 +0700

    Update help command

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -23,6 +23,6 @@ async def help(event):
             \nUsage: .help <module name>")
         string = ""
         for i in CMD_HELP:
-            string += "'" + str(i)
-            string += "🖕"
+            string += "`" + str(i)
+            string += "🔹"
         await event.reply(string)
